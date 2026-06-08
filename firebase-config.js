373535421d23430a2e227152604b46f85221d487
Abbati Revanth Reddy <108979779+RevanthReddyAbbati@users.noreply.github.com>
2026-06-08 11:40:54 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ══════════════════════════════════════════════════════════════
+// VYTARA — Firebase Configuration File
+// ══════════════════════════════════════════════════════════════
+const firebaseConfig = {
+  apiKey: "AIzaSyAeHZRyQcAXAIKD2bW72IF_mEIIdCfH0dI",
+  authDomain: "gym-tracker-ec342.firebaseapp.com",
+  projectId: "gym-tracker-ec342",
+  storageBucket: "gym-tracker-ec342.firebasestorage.app",
+  messagingSenderId: "799478199566",
+  appId: "1:799478199566:web:1ae71f357400d9795c135f",
+  measurementId: "G-YEWVD5LZZV"
+};
+// ══════════════════════════════════════════════════════════════
